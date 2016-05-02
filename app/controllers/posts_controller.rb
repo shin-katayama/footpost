715975baa4a60fbe88cc364d514ba8c07393daab
shin-katayama <peko775kuro@gmail.com>
2016-05-02 16:37:25 +0900

    ログイン、新規登録、を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
 
+  before_action :move_to_index, except: :index
+
   def index
     @posts = Post.order("id DESC")
   end
@@ -14,6 +16,10 @@ class PostsController < ApplicationController
   private
   def post_params
     params.permit(:name, :image, :text)
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
 
 
