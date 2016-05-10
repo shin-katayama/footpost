2173b67ad457372763ea307c4961fba5cddd6f2f
shin-katayama <peko775kuro@gmail.com>
2016-05-10 10:28:05 +0900

    投稿にコメント欄を設置

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'posts#index'
-  get 'posts' => 'posts#index'
-  get 'posts/new'  => 'posts#new'       #投稿画面
-  post 'posts'      => 'posts#create'    #投稿機能
-  delete  'posts/:id'  => 'posts#destroy'
-  patch   'posts/:id'  => 'posts#update'
-  get   'posts/:id/edit'  => 'posts#edit'
-  get   'users/:id'   =>  'users#show'    #Mypageへのルーティング
-  get 'posts/:id' => 'posts#show'
+  resources :posts do
+    resources :comments, only: [:create]
+  end
+  resources :users, only: [:show]
 end
