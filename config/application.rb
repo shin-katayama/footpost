9b68d78fa07d4fba268c08f66ef2fdbbf2adda86
shin-katayama <peko775kuro@gmail.com>
2016-05-11 11:09:30 +0900

    bootstrapを入れた

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module Footpost
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.paths << "#{Rails}/vender/assets/fonts"
   end
 end
